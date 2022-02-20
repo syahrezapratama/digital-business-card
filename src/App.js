35544f144f70b3b1c3a6d1b454bc1283cc13335b
syahr <syahrezaraditya@gmail.com>
2022-02-20 14:42:09 +0100

    add css styling

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <Picture />
-      <Content />
-      <Footer />
+      <div className="card">
+        <Picture />
+        <Content />
+        <Footer />
+      </div> 
     </div>
   );
 }
